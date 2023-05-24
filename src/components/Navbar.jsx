08cc191aaffd07fa858950ff14f27c778df90152
F-Z-C <Faisal_Chaudhry235@hotmail.ca>
2023-05-24 03:07:38 -0400

    added a navigation bar for desktop, a hamburger menu for mobile included react-router-dom for the navigation added a website logo, removed a few uneeded packaged from json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { menu, close, flogo } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    // Using a Semantic tag
+    // Within the sematic tag, there contains a special utility claass.
+    <nav
+      className={` ${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+        
+          to="/"
+          className="flex items-cetner gap-2"
+          onClick={() => {
+            setActive(" ");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <div className="flex items-center  ">
+            <img
+              src={flogo}
+              alt="logo"
+              className=" w-30 h-24 object-contain "
+            />
 
-export default Navbar
+            <p className="text-white text-[34px] font-bold cursor-pointer">
+              <span className="sm:block hidden mx-[-24px] ">aisal</span>
+            </p>
+          </div>
+        </Link>
+
+        <ul className="list-none sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-red-400" : "text-secondary"
+              } hover:text-white text-[20px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              {/* Dynamic template string */}
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile Navigation */}
+        <div className="sm:flex sm:bg-transparent md:hidden ">
+          <img
+            //  if toggle, then close : else show menu
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[40px] h-[40px] object-contain cursor-pointer "
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            {/* if it's not toggled show hidden : else show flex */}
+            <ul className="list-none flex justify-end items-start flex-col gap-6">
+              {/* Dynamic block of code  */}
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary font-poppins font-medium"
+                } hover:text-white text-[20px] cursor-pointer`}
+                  onClick={() =>  {
+
+                    setToggle(!toggle);
+                    setActive(link.title)
+                  
+                  }}
+                >
+                  {/* Dynamic template string */}
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+    
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
