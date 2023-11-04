f182471df134403d9ba64f41e7856686b10e3254
F-Z-C <Faisal_Chaudhry235@hotmail.ca>
2023-11-03 22:41:18 -0400

    testing remove favicon since aws amplify is not reading it correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, ComputersCanvas} from './components'
-import { OrangeMushroomCanvas } from "./components/canvas"
 
 const App = () => {
   return (
