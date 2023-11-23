fd0be823decf86a18fc3cc9ad63123c2b90e7d44
F-Z-C <Faisal_Chaudhry235@hotmail.ca>
2023-11-23 17:06:49 -0500

    Changed the dpr (device pixel ratio) to a lower value, The higher value added beneficial texture improvement on the character, but it added a significant amount of load in compute resources, the trade was not worth it. Will tune in the near future if need be.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,6 @@ import { profile, youtube, github, youtube_circle } from "../assets";
 import { Thief } from "./canvas";
 import { Canvas } from "@react-three/fiber";
 
-
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -63,18 +62,17 @@ const Contact = () => {
     <div className="xl::mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl" >
-       
-    <div className="absolute justify-end right-8 ">
-    <img
-              src={profile}
-              alt="profile"
-              className="rounded-full object-cover w-16 h-16 xs:w-20 xs:h-20 transition-all ease-in-out duration-1000" 
-            />
-            
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <div className="absolute justify-end right-8 ">
+          <img
+            src={profile}
+            alt="profile"
+            className="rounded-full object-cover w-16 h-16 xs:w-20 xs:h-20 transition-all ease-in-out duration-1000"
+          />
         </div>
         <h2 className={styles.sectionHeadText}>Connect</h2>
-      
+
         <form
           ref={formRef}
           onSubmit={handleSubmit}
@@ -116,52 +114,54 @@ const Contact = () => {
             />
           </label>
 
-  <div className="flex items-center justify-between">
-  <button
-    type="submit"
-    className="bg-tertiary py-3 px-10 outline-none w-fit text-white font-bold shadow-2xl shadow-primary rounded-lg"
-  >
-    {loading ? "Sending..." : "Send"}
-  </button>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-tertiary py-3 px-10 outline-none w-fit text-white font-bold shadow-2xl shadow-primary rounded-lg"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
 
-  <div className="flex items-center space-x-2">
-    <a href="https://github.com/FazeNCode" target="_blank" rel="noopener noreferrer">
-    <img
-      src={github}
-      alt="profile"
-      className="w-10 h-10 sm:w-14 sm:h-14 rounded-full object-cover"
-    />
-    </a>
-    
-    {/* ! IMPORT A NEW youtube_circle WITH A SMALLER SIZE TO MATCH GITHUB LOGO! */}
-    <img
-      src={youtube_circle}
-      alt="profile"
-      className="w-10 h-10 sm:w-14 sm:h-14 rounded-full object-cover"
-    />
-  </div>
-</div>
+            <div className="flex items-center space-x-2">
+              <a
+                href="https://github.com/FazeNCode"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={github}
+                  alt="profile"
+                  className="w-10 h-10 sm:w-14 sm:h-14 rounded-full object-cover"
+                />
+              </a>
+
+              {/* ! IMPORT A NEW youtube_circle WITH A SMALLER SIZE TO MATCH GITHUB LOGO! */}
+              <img
+                src={youtube_circle}
+                alt="profile"
+                className="w-10 h-10 sm:w-14 sm:h-14 rounded-full object-cover"
+              />
+            </div>
+          </div>
         </form>
-
       </motion.div>
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[450px] h-[350px"
-    >
-      <div className=" h-[600px]">
-<Canvas
-dpr={[8, 8]}
-camera={{
-  fov: 45,
-  near: 5,
-  far: -200,
-  position: [-2, 3, 6],
-}}
->
-    <Thief/>
-    </Canvas>
-</div>
-          
+      >
+        <div className="h-full ">
+          <Canvas
+            dpr={[4, 4]}
+            camera={{
+              fov: 45,
+              near: 5,
+              far: -200,
+              position: [-2, 3, 6],
+            }}
+          >
+            <Thief />
+          </Canvas>
+        </div>
       </motion.div>
     </div>
   );
