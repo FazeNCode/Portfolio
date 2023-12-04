96c210fb437d0236b5707ffcaee2f9897606d29f
F-Z-C <Faisal_Chaudhry235@hotmail.ca>
2023-12-04 15:28:20 -0500

    Issue with new scene not being found, loaded old scene to test

--- src/components/canvas/MushroomHouse.jsx
+++ src/components/canvas/MushroomHouse.jsx
@@ -6,7 +6,7 @@ import { useFrame } from '@react-three/fiber';
 
 const MushroomHouse = () => {
   const group = useRef();
-  const { nodes, animations } = useGLTF('./mushroomhouse_scene/mushroomtown.glb');
+  const { nodes, animations } = useGLTF('./mushroomhouse_scene/MushroomTown79.glb');
   const { setAnimations, animationIndex } = useCharacterAnimations();
   const { actions, names } = useAnimations(animations, group);
 
